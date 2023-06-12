1c8f3d5e78bf6ed069ae9d24187067f09465c041
vagner0795 <vagner-se@hotmail.com>
2023-06-12 10:57:52 -0300

    feat: created file.js and function to convert values

---
+++ script.js
@@ -0,0 +1,48 @@
+const input = document.querySelector('.input-currency') //! input
+const convertButton = document.querySelector('.convert-button') //! button
+const currencyValueToConvert = document.querySelector('.currency-value-to-convert') //! value to convert
+const currencyValueConverted = document.querySelector('.currency-value') //! value converted
+const currencySelect = document.querySelector('.currency-select')
+
+
+//? Intl.NumberFormat - Javascript Library to format values
+
+// format values function
+function convertValues(){
+    const inputCurrencyValue = input.value
+
+    const dolarToday = 4.8
+    const euroToday = 6.3
+
+
+    if(currencySelect.value == 'dolar') {
+        currencyValueConverted.innerHTML = new Intl.NumberFormat("en-US", {
+            style: "currency",
+            currency: "USD"
+        }).format(inputCurrencyValue / dolarToday)
+    }
+
+    if(currencySelect.value == 'euro'){
+        currencyValueConverted.innerHTML = new Intl.NumberFormat("de-DE", {
+            style: "currency",
+            currency: "EUR"
+        }).format( inputCurrencyValue / euroToday)
+
+    }
+
+    currencyValueToConvert.innerHTML = new Intl.NumberFormat("pt-BR", {
+        style: "currency",
+        currency: "BRL"
+    }).format(inputCurrencyValue)
+
+
+
+
+
+
+
+
+}
+
+
+convertButton.addEventListener('click', convertValues)
